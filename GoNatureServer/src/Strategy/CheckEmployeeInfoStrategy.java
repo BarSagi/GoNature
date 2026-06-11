9aeda99c09a22746d6ae00eb64cf35323f330065
Gal-Dolev <galdolev12@gmail.com>
2026-06-11 19:35:47 +0300

    added order stuff name works in order screen

--- GoNatureServer/src/Strategy/CheckEmployeeInfoStrategy.java
+++ GoNatureServer/src/Strategy/CheckEmployeeInfoStrategy.java
@@ -11,26 +11,34 @@ public class CheckEmployeeInfoStrategy implements MessageStrategy {
 	@Override
 	public void execute(Message msg, ConnectionToClient client, EchoServer server) throws Exception {
 		try {
+			// 1. Extract the raw data (ArrayList of Strings) from the client's message
 			@SuppressWarnings("unchecked")
 			ArrayList<String> employeeData = (ArrayList<String>) msg.getData();
 
-			ArrayList<String> fullEmployeeData = server.getDatabase().getEmployeeInfo(employeeData);
+			// 2. Pass the data to the DBController to execute the INSERT query
+			String role = server.getDatabase().getEmployeeRole(employeeData);
 
-			Message responseMsg = new Message("EMPLOYEE_ROLE_RESULT", fullEmployeeData);
+			// 3. Package the boolean result into a new Message and send it back to the
+			// client
+			Message responseMsg = new Message("EMPLOYEE_ROLE_RESULT", role);
 			client.sendToClient(responseMsg);
 
-			System.out.println("Server: Employee info fetch attempt for username "
-					+ employeeData.get(0) + " resulted in: " + fullEmployeeData);
+			// Print to server console for debugging
+			System.out.println("Server: Role fetch attempt attempt for employee's username " + employeeData.get(0) + " resulted in: "
+					+ role);
 
 		} catch (Exception e) {
 			System.out.println("Server Error: Failed to execute CheckEmployeeInfoStrategy.");
 			e.printStackTrace();
 
+			// Optionally send a failure message back to the client if an exception occurs
 			try {
 				client.sendToClient(new Message("EMPLOYEE_ROLE_RESULT", null));
 			} catch (Exception ex) {
 				ex.printStackTrace();
 			}
 		}
+
 	}
+
 }
--- GoNatureServer/src/Strategy/StrategyFactory.java
+++ GoNatureServer/src/Strategy/StrategyFactory.java
@@ -24,27 +24,6 @@ public class StrategyFactory {
 		case "CHECK_EMPLOYEE_INFO":
 			return new CheckEmployeeInfoStrategy();
 
-		case "GET_VISIT_REPORT":
-		    return new GetVisitReportStrategy();
-		    
-		case "ENTER_VISITOR":
-		    return new EnterVisitorStrategy();
-
-		case "EXIT_VISITOR":
-		    return new ExitVisitorStrategy();
-		    
-		case "REGISTER_FAMILY_SUBSCRIBER":
-		    return new RegisterFamilySubscriberStrategy();
-
-		case "REGISTER_GROUP_GUIDE":
-		    return new RegisterGroupGuideStrategy();
-		
-		case "SUBMIT_PARK_REQUEST":
-		    return new SubmitParkRequestStrategy();
-		    
-		case "GET_PARK_CURRENT_VALUE":
-		    return new GetParkCurrentValueStrategy();
-		    
 		default:
 			return null;
 		}
